34cbc0e60ae1f5865c3164fae6ec217728184906
Jim <jimkaraboutas@gmail.com>
2022-11-24 17:28:10 +0200

    Comment

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -44,6 +44,7 @@ namespace WinFormsApp1
       this.button1 = new System.Windows.Forms.Button();
       this.button2 = new System.Windows.Forms.Button();
       this.button3 = new System.Windows.Forms.Button();
+      this.label7 = new System.Windows.Forms.Label();
       this.SuspendLayout();
       // 
       // label1
@@ -51,12 +52,13 @@ namespace WinFormsApp1
       this.label1.AutoSize = true;
       this.label1.Location = new System.Drawing.Point(12, 34);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(38, 15);
+      this.label1.Size = new System.Drawing.Size(52, 15);
       this.label1.TabIndex = 1;
-      this.label1.Text = "label1";
+      this.label1.Text = "Strength";
       // 
       // textBox1
       // 
+      this.textBox1.Enabled = false;
       this.textBox1.Location = new System.Drawing.Point(135, 34);
       this.textBox1.Name = "textBox1";
       this.textBox1.Size = new System.Drawing.Size(100, 23);
@@ -64,6 +66,7 @@ namespace WinFormsApp1
       // 
       // textBox2
       // 
+      this.textBox2.Enabled = false;
       this.textBox2.Location = new System.Drawing.Point(135, 87);
       this.textBox2.Name = "textBox2";
       this.textBox2.Size = new System.Drawing.Size(100, 23);
@@ -74,12 +77,13 @@ namespace WinFormsApp1
       this.label2.AutoSize = true;
       this.label2.Location = new System.Drawing.Point(12, 87);
       this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(38, 15);
+      this.label2.Size = new System.Drawing.Size(54, 15);
       this.label2.TabIndex = 3;
-      this.label2.Text = "label2";
+      this.label2.Text = "Dexterity";
       // 
       // textBox3
       // 
+      this.textBox3.Enabled = false;
       this.textBox3.Location = new System.Drawing.Point(135, 143);
       this.textBox3.Name = "textBox3";
       this.textBox3.Size = new System.Drawing.Size(100, 23);
@@ -90,12 +94,13 @@ namespace WinFormsApp1
       this.label3.AutoSize = true;
       this.label3.Location = new System.Drawing.Point(12, 143);
       this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(38, 15);
+      this.label3.Size = new System.Drawing.Size(73, 15);
       this.label3.TabIndex = 5;
-      this.label3.Text = "label3";
+      this.label3.Text = "Constitution";
       // 
       // textBox4
       // 
+      this.textBox4.Enabled = false;
       this.textBox4.Location = new System.Drawing.Point(135, 198);
       this.textBox4.Name = "textBox4";
       this.textBox4.Size = new System.Drawing.Size(100, 23);
@@ -106,12 +111,13 @@ namespace WinFormsApp1
       this.label4.AutoSize = true;
       this.label4.Location = new System.Drawing.Point(12, 198);
       this.label4.Name = "label4";
-      this.label4.Size = new System.Drawing.Size(38, 15);
+      this.label4.Size = new System.Drawing.Size(68, 15);
       this.label4.TabIndex = 7;
-      this.label4.Text = "label4";
+      this.label4.Text = "Intelligence";
       // 
       // textBox5
       // 
+      this.textBox5.Enabled = false;
       this.textBox5.Location = new System.Drawing.Point(135, 254);
       this.textBox5.Name = "textBox5";
       this.textBox5.Size = new System.Drawing.Size(100, 23);
@@ -122,12 +128,13 @@ namespace WinFormsApp1
       this.label5.AutoSize = true;
       this.label5.Location = new System.Drawing.Point(12, 254);
       this.label5.Name = "label5";
-      this.label5.Size = new System.Drawing.Size(38, 15);
+      this.label5.Size = new System.Drawing.Size(51, 15);
       this.label5.TabIndex = 9;
-      this.label5.Text = "label5";
+      this.label5.Text = "Wisdom";
       // 
       // textBox6
       // 
+      this.textBox6.Enabled = false;
       this.textBox6.Location = new System.Drawing.Point(135, 306);
       this.textBox6.Name = "textBox6";
       this.textBox6.Size = new System.Drawing.Size(100, 23);
@@ -138,18 +145,19 @@ namespace WinFormsApp1
       this.label6.AutoSize = true;
       this.label6.Location = new System.Drawing.Point(12, 306);
       this.label6.Name = "label6";
-      this.label6.Size = new System.Drawing.Size(38, 15);
+      this.label6.Size = new System.Drawing.Size(57, 15);
       this.label6.TabIndex = 11;
-      this.label6.Text = "label6";
+      this.label6.Text = "Charisma";
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(439, 397);
+      this.button1.Location = new System.Drawing.Point(387, 61);
       this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.Size = new System.Drawing.Size(107, 49);
       this.button1.TabIndex = 13;
-      this.button1.Text = "button1";
+      this.button1.Text = "Roll Dice";
       this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click_1);
       // 
       // button2
       // 
@@ -157,8 +165,9 @@ namespace WinFormsApp1
       this.button2.Name = "button2";
       this.button2.Size = new System.Drawing.Size(75, 23);
       this.button2.TabIndex = 14;
-      this.button2.Text = "button2";
+      this.button2.Text = "Reset";
       this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
       // 
       // button3
       // 
@@ -166,14 +175,25 @@ namespace WinFormsApp1
       this.button3.Name = "button3";
       this.button3.Size = new System.Drawing.Size(75, 23);
       this.button3.TabIndex = 15;
-      this.button3.Text = "button3";
+      this.button3.Text = "Continue";
       this.button3.UseVisualStyleBackColor = true;
+      // 
+      // label7
+      // 
+      this.label7.AutoSize = true;
+      this.label7.Location = new System.Drawing.Point(548, 78);
+      this.label7.Name = "label7";
+      this.label7.Size = new System.Drawing.Size(38, 15);
+      this.label7.TabIndex = 16;
+      this.label7.Text = "label7";
+      this.label7.Visible = false;
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.label7);
       this.Controls.Add(this.button3);
       this.Controls.Add(this.button2);
       this.Controls.Add(this.button1);
@@ -213,6 +233,7 @@ namespace WinFormsApp1
     private System.Windows.Forms.Button button1;
     private System.Windows.Forms.Button button2;
     private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Label label7;
   }
 }
 
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -12,10 +12,75 @@ namespace WinFormsApp1
 {
   public partial class Form1 : Form
   {
+    int index = 0;
+
     public Form1()
     {
       InitializeComponent();
     }
+    int roll()
+    {
+      int sum = 0;
+      int dice = 0;
+      Random rand = new Random();
+      label7.Text = "";
+      if (label7.Visible == false)
+      {
+        label7.Visible = true;
+      }
+      for (int i = 0; i < 3; i++)
+      {
+        dice = rand.Next(1, 6);
+        if ( i == 2)
+        {
+          label7.Text += dice;
+        }
+        else
+        {
+          label7.Text += string.Format("{0} + ", dice);
+        }
+        sum += dice;
+      }
 
+      return sum;
+    }
+
+    private void button1_Click_1(object sender, EventArgs e)
+    {
+      switch (index)
+      {
+        case 0:
+          textBox1.Text = roll().ToString();
+          break;
+        case 1:
+          textBox2.Text = roll().ToString();
+          break;
+        case 2:
+          textBox3.Text = roll().ToString();
+          break;
+        case 3:
+          textBox4.Text = roll().ToString();
+          break;
+        case 4:
+          textBox5.Text = roll().ToString();
+          break;
+        case 5:
+          textBox6.Text = roll().ToString();
+          break;
+      }
+      index++;
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      index = 0;
+      textBox1.Text = "";
+      textBox2.Text = "";
+      textBox3.Text = "";
+      textBox4.Text = "";
+      textBox5.Text = "";
+      textBox6.Text = "";
+      label7.Visible = false;
+    }
   }
 }
